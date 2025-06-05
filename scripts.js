bc35c481cf8ca2d76272473e17669a8340ceb7ef
iago <iagoamaral033>
2025-06-05 15:29:18 -0300

    atualização

--- scripts.js
+++ scripts.js
@@ -9,20 +9,22 @@ function lerConteudoDoArquivo(arquivo){
     return new Promise((resolve, reject) => {
         const leitor = new FileReader();
         leitor.onload = () => {
-            resolve({ URL: leitor.result, nome: arquivo.name })
-        }
+            resolve({ URL: leitor.result, nome: arquivo.name });
+        };
         leitor.onerror = () => {
-            reject('Erro na leitura do arquivo ${arquivo.name}')
-        }
-        leitor.readAsDataURL(arquivo)
+            reject('Erro na leitura do arquivo ${arquivo.name}');
+        };
+        leitor.readAsDataURL(arquivo);
 
-    })
+    });
 }
 
 const imagemPricipal = document.querySelector (".main-imagem");
 const nomeDaImagem = document.querySelector(".cotainer-imagem-nome p");
+const inputUpload = document.getElementById("input-upload");
 
 inputUpload.addEventListener("change", async (evento) => {
+    
     const arquivo = evento.target.Files[0];
 
     if (arquivo) {
